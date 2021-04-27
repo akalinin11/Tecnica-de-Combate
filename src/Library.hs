b10192e949005f7aa2baa15b510f7b2a5d328033
Alexander Kalinin <akalinin@frba.utn.edu.ar>
2021-04-26 22:47:40 -0300

    Trabajo Tecnica de Combate subido

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,67 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+-- Punto 1
+
+type CantidadHs = Number 
+type NombreObjeto = String
+type Presion = Number
+
+
+presionDeUnGolpe :: CantidadHs-> NombreObjeto -> Presion
+presionDeUnGolpe horas nombre =   poderGolpe horas / fortaleza nombre
+
+poderGolpe :: CantidadHs->Number  --Paso las horas y realizo la multiplicacion
+poderGolpe= (*15)
+
+fortaleza :: NombreObjeto->Number  --Paso el nombre del objeto y primero se calcula la cantidad de letras para despues multiplicarlo por 2
+fortaleza= (2*).length
+
+
+--Punto 2
+
+type GolpeGomu = NombreObjeto -> Presion  {-Realizamos esto para simplificar la definicion de la funcion (? // Ya que GolpeGomu va a recibir solo 
+                                           un string (nombreObjeto) y va a devolver la presion,ya que las horas ya estan "definidas"-}
+
+gomuGomu :: GolpeGomu
+gomuGomu = presionDeUnGolpe 180
+
+
+type GolpesNormalesConsec = NombreObjeto -> Presion
+
+golpesNormCons :: GolpesNormalesConsec  --Golpes normales consecutivos, mismo pensamiento que golpes gomugomu
+golpesNormCons = presionDeUnGolpe 240
+
+
+-- Punto 3 
+
+objDificil :: NombreObjeto->Bool
+objDificil= (<100).gomuGomu
+
+{-comp :: Number->Bool  pensado sin aplicacion parcial y composicion
+comp x = x < 100 -}
+
+objAccesible :: NombreObjeto->Bool
+objAccesible = objetivoEntre.golpesNormCons.focalizar7
+
+objetivoEntre :: Number->Bool
+objetivoEntre= between 200 400  
+
+between :: Number-> Number->Number->Bool                    
+between bajo alto medio =  bajo <= medio && medio <= alto
+
+focalizar7 :: NombreObjeto -> NombreObjeto  --focalizar7: quedarse con las primeras 7 letras de su nombre
+focalizar7 = take 7
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/Spec.hs
+++ src/Spec.hs
@@ -5,7 +5,23 @@ import Test.Hspec
 
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "Test de ejemplo" $ do
-    it "El pdepreludat se instaló correctamente" $ do
-      doble 1 `shouldBe` 2
+  describe "Testeo del punto 1: presionDeUnGolpe" $ do   --Parte del punto 1 testeada ya que no hay otros tipos de resultados
+    it "La presion de un golpe con 200hs de entrenamiento sobre una bolsa de entrenamiento deberia ser 68" $ do
+      presionDeUnGolpe 200 "Bolsa de Entrenamiento" `shouldBe` 68.181818182
+  describe "Testeo del punto 2: Golpe gomu gomu elephant gatling" $ do  
+    it "La presion de un golpe gomo gomu... sobre la bolsa de entrenamineto deberia ser 61" $ do
+      gomuGomu  "Bolsa de Entrenamiento"  `shouldBe` 61.363636364
+  describe "Testeo del punto 2: Golpes normales consecutivos" $ do  
+    it "La presion de unos golpes normales consecutivos sobre la bolsa deberia ser 81 " $ do
+      golpesNormCons "Bolsa de Entrenamiento"  `shouldBe` 81.818181818
+  describe "Testeo del punto 3: es dificil un objetivo?" $ do  
+    it "La bolsa de entrenamiento es un objetivo dificil " $ do
+      objDificil "bolsa de entrenamiento"  `shouldBe` True
+    it "El puf no es un objetivo dificil" $ do
+     objDificil "Puf" `shouldBe` False
+  describe "Testeo del punto 3: es accesible un objetivo?" $ do
+    it "Una bolsa de entrenamiento como objetivo es accesible" $ do
+      objAccesible "Bolsa de entranamiento" `shouldBe` True
+    it "Un puf como objetivo no es accesible" $ do
+      objAccesible "puf" `shouldBe` False 
 
